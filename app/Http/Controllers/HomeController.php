d2b667739b93d8e75ab412f8d53bd578cb912495
Tohidul Islam <raptor.dnj@gmail.com>
2023-08-02 19:09:40 +0600

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Transaction;
+use App\UserInPackage;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -26,7 +27,8 @@ class HomeController extends Controller
         $userId = auth()->id();
 
         $userBalance = Transaction::where('user_id',  $userId)->sum('amount');
+        $packageCount = UserInPackage::where('user_id',  $userId)->count();
         // dd( $userBalance);
-        return view('home',compact('userBalance'));
+        return view('home',compact('userBalance','packageCount'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                     </div>
                     <p class="mt-3 mb-0 text-sm">
                         <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
+                        <span class="text-nowrap">Since today</span>
                     </p>
                 </div>
             </div>
@@ -31,8 +31,8 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                            <span class="h2 font-weight-bold mb-0">2,356</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Packages</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $packageCount }}</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
