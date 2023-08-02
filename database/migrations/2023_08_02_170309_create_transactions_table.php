75ca6120b8896945aac9857f8a19f6f392900b10
Tohidul Islam <raptor.dnj@gmail.com>
2023-08-02 18:12:43 +0600

    Package, transaction added

--- database/migrations/2023_08_02_170309_create_transactions_table.php
+++ database/migrations/2023_08_02_170309_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserInPackagesTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUserInPackagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_in_packages', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('package_id');
+            $table->tinyInteger('tr_type')->unsigned();
+            $table->string('particular')->nullable();
             $table->foreignId('user_id');
-            $table->tinyInteger('status')->default(1);
+            $table->decimal('amount',10,2);
             $table->timestamps();
             $table->softDeletes();
         });
@@ -30,6 +31,6 @@ class CreateUserInPackagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_in_packages');
+        Schema::dropIfExists('transactions');
     }
 }
